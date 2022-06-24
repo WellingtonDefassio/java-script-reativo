d108a55b39580153b0df235102318dc6e387a139
Wellington Defassio <well.defassio@gmail.com>
2022-06-24 17:09:24 -0300

    commit basico sobre funcoes

---
+++ function/basico_2.js
@@ -0,0 +1,67 @@
+function bomDia() {
+    console.log('Bom dia!!!')
+}
+const boaTarde = function boaTardeNome (){
+    console.log('Boa tarde!!!')
+}
+
+function executarQualquerCoisa(fn) {
+        if(typeof fn === 'function') {
+            fn()
+        }
+}
+
+
+executarQualquerCoisa(3)
+executarQualquerCoisa(bomDia())
+executarQualquerCoisa(boaTarde())
+
+
+// 2) retornar uma função a partir e uma outra função
+
+function potencia(base){
+    return function(exp) {
+        return Math.pow(base, exp)
+    }
+}
+
+const potenciaDe2 = potencia(2)
+console.log(potencia)
+
+const resolvido = potenciaDe2(8)
+
+console.log(resolvido)
+
+console.log(potencia(3)(3))
+
+
+console.log('DESAFIO')
+
+const somar = function(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c 
+        }
+    }
+}
+
+console.log(somar(2)(2)(2))
+
+
+const calcular = function(a){
+  return function(b) {
+       return function(fn){
+            return fn(a,b)
+       }
+    }
+}
+
+function multiplicar(a, b){
+    return a * b
+}
+function somar2(a,b){
+    return a + b
+}
+
+console.log(calcular(10)(5)(multiplicar))
+console.log(calcular(5)(6)(somar2))
